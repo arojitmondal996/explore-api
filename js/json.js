e8efb3fb74e68ae6beda308f9c01e5d8815e97f5
arojitmondal996e <arojitmondal996@gmail.com>
2024-10-01 11:27:46 +0600

    first commit

---
+++ js/json.js
@@ -0,0 +1,29 @@
+const user = {Id: 1, name: 'Murad Hasan', job: 'Doctor'};
+// JavaScript Object Notation (JSON)
+const stringified = JSON.stringify(user);
+
+// console.log(user);
+// console.log(stringified);
+
+// { Id: 1, name: 'Murad Hasan', job: 'Doctor' }
+// {"Id":1,"name":"Murad Hasan","job":"Doctor"}
+
+const shop = {
+    owner: 'Alia',
+    address: {
+        street: 'kochukhet voot er goli',
+        city: 'ranbir',
+        country: 'BD',
+    },
+
+    products: ['laptop', 'mic', 'monitor', 'keyboard'],
+    revenue: 45000,
+    isOpen: true,
+    isNew: false
+}
+
+console.log(shop);
+const shopJSON = JSON.stringify(shop);
+console.log(shopJSON);
+const shopObj = JSON.parse(shopJSON);
+console.log(shopObj)
